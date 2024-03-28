8be43942a0021a12dfda456f4b5911b3060c8afd
deedatbilla <deedat5@yahoo.com>
2024-03-28 15:20:08 +0000

    Add Suspense component to improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Box, Text } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function Home() {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
@@ -25,36 +26,38 @@ export default function Home() {
   }, [code]);
 
   return (
-    <Box
-      bg="#FBFBFC"
-      height={"100vh"}
-      overflowY={"scroll"}
-      position={"relative"}
-    >
-      <Header />
-      <Box mx={"auto"} maxW={"1100px"} py={10} px={4}>
-        <Text fontSize={"28px"} fontWeight={"bold"}>
-          Settings
-        </Text>
+    <Suspense>
+      <Box
+        bg="#FBFBFC"
+        height={"100vh"}
+        overflowY={"scroll"}
+        position={"relative"}
+      >
+        <Header />
+        <Box mx={"auto"} maxW={"1100px"} py={10} px={4}>
+          <Text fontSize={"28px"} fontWeight={"bold"}>
+            Settings
+          </Text>
 
-        <Box mt={5}>
-          <Tabs>
-            <TabList>
-              <Tab>Account</Tab>
-              <Tab>User management</Tab>
-            </TabList>
+          <Box mt={5}>
+            <Tabs>
+              <TabList>
+                <Tab>Account</Tab>
+                <Tab>User management</Tab>
+              </TabList>
 
-            <TabPanels mt={5}>
-              <TabPanel>
-                <AccountTab />
-              </TabPanel>
-              <TabPanel>
-                <UserManagementTab />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+              <TabPanels mt={5}>
+                <TabPanel>
+                  <AccountTab />
+                </TabPanel>
+                <TabPanel>
+                  <UserManagementTab />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </Suspense>
   );
 }
