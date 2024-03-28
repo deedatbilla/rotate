d94acd591c33b32ab64ae69eb5bc784ab756ea08
deedatbilla <deedat5@yahoo.com>
2024-03-28 16:36:24 +0000

    Refactor Header component layout and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,31 +6,31 @@ import { useAuth } from "@/context/authContext";
 function Header() {
   const { user, logout } = useAuth();
   return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      position={"sticky"}
-      top={0}
-      p={5}
-      zIndex={100}
-      bg={"white"}
-    >
-      <Logo width={150} />
-      {user && (
-        <Box display={"flex"} alignItems={"center"} gap={3}>
-          <Box>
-            <Text fontSize="16px" fontWeight="bold">
-              {user?.name}
-            </Text>
-            <Text fontSize="14px" fontWeight="400">
-              {user?.email}
-            </Text>
-          </Box>
+    <Box position={"sticky"} top={0} bg={"white"} p={5}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        maxW={"1200px"}
+        mx={"auto"}
+        zIndex={100}
+      >
+        <Logo width={150} />
+        {user && (
+          <Box display={"flex"} alignItems={"center"} gap={3}>
+            <Box>
+              <Text fontSize="16px" fontWeight="bold">
+                {user?.name}
+              </Text>
+              <Text fontSize="14px" fontWeight="400">
+                {user?.email}
+              </Text>
+            </Box>
 
-          <Avatar name={user?.name} src={user?.picture} />
-        </Box>
-      )}
+            <Avatar name={user?.name} src={user?.picture} />
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 }
